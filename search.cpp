abb676fd4cd65792a7dfbff99c996407cc0bd941
luigirovani <luigi3520@gmail.com>
2024-11-05 19:16:09 -0300

    modificador para usar random para gerar os números

--- search.cpp
+++ search.cpp
@@ -1,80 +1,122 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+﻿//Luigi Augusto Rovani
+//Maria Julia Leandro Leal da Rocha
+//Pedro Augusto De Lima Oliveira
+//Vithoria Cabreira
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <numeric>
+#include <random>
+#include <locale>
+#include <limits>
+
+
+
+
+const int min_value = 0;
+const int sample = 100;
+const int min_length = 10;
+const auto max_length = 100000000; // 1 bilhão
 
 using std::vector;
 using std::cout;
 using std::endl;
-using std::srand;
 
+class Busca {
+protected:
+    long int count;
 
-static void buscaSequencial(const vector<int>& vetor, int& count, int elemento) {
-    for (size_t i = 0; i < vetor.size(); ++i) {
-        count++;
-        if (vetor[i] == elemento) {
-            break;
+public:
+    Busca() : count(0) {}
+
+    virtual void buscar(const vector<int>& vetor, int elemento) = 0;
+
+    void testar(const vector<int>& vetor, int tentativas) {
+        count = 0;
+        std::random_device rd;
+        std::mt19937 gerador(rd());
+        std::uniform_int_distribution<int> distribuicao(0, vetor.size() - 1);
+
+        for (int i = 0; i < tentativas; i++) {
+            int elemento = vetor[distribuicao(gerador)];
+            buscar(vetor, elemento);
+        }
+        int media = count / tentativas;
+        cout << " " << media << endl;
+    }
+
+};
+
+class BuscaSequencial : public Busca {
+public:
+    void buscar(const vector<int>& vetor, int elemento) override {
+        for (size_t i = 0; i < vetor.size(); ++i) {
+            count++;
+            if (vetor[i] == elemento) {
+                break;
+            }
         }
     }
-}
+};
 
-static void buscaBinaria(const vector<int>& vetor, int& count, int elemento) {
-    int inicio = 0;
-    int fim = vetor.size() - 1;
+class BuscaBinaria : public Busca {
+public:
+    void buscar(const vector<int>& vetor, int elemento) override {
+        int inicio = 0;
+        int fim = vetor.size() - 1;
 
-    while (inicio <= fim) {
-        count++;
-        int meio = inicio + (fim - inicio) / 2;
+        while (inicio <= fim) {
+            count++;
+            int meio = inicio + (fim - inicio) / 2;
 
-        if (vetor[meio] == elemento)
-            break;
+            if (vetor[meio] == elemento)
+                break;
 
-        if (vetor[meio] < elemento)
-            inicio = meio + 1;
-        else
-            fim = meio - 1;
+            if (vetor[meio] < elemento)
+                inicio = meio + 1;
+            else
+                fim = meio - 1;
+        }
+    }
+};
+
+static vector<int> gen_vector(int len_vector) {
+    vector<int> vetor(len_vector);
+
+    std::random_device rd;
+    std::mt19937 gerador(rd());
+    std::uniform_int_distribution<int> distribuicao(min_value, len_vector * 10);
+
+    for (int& valor : vetor) {
+        valor = distribuicao(gerador);
     }
 
+    std::sort(vetor.begin(), vetor.end());
+    return vetor;
 }
 
-static void binaria(const vector<int>& vetor, int tentativas) {
-	int count = 0;
+static void test(int len_vector) {
+    vector<int> vetor = gen_vector(len_vector);
+    cout << "Testando com vetor de tamanho: " << len_vector << endl;
 
-    for (int i = 0; i < tentativas; i++) {
-        int elemento = vetor[std::rand() % vetor.size()];
-        buscaBinaria(vetor, count, elemento);
-    }
-    int media = count / tentativas;
-    cout << "Media da busca binaria: " << media << endl;
+    BuscaSequencial buscaSeq;
+    cout << "Média Busca Sequencial: ";
+    buscaSeq.testar(vetor, sample);
+
+    BuscaBinaria buscaBin;
+    cout << "Média Busca Binária: ";
+    buscaBin.testar(vetor, sample);
+
+    cout << endl;
 }
-
-static void sequencial(const vector<int>& vetor, int tentativas) {
-    int count = 0;
-
-    for (int i = 0; i < tentativas; i++) {
-        int elemento = vetor[std::rand() % vetor.size()];
-        buscaSequencial(vetor, count, elemento);
-    }
-    int media = count / tentativas;
-    cout << "Media da busca sequencial: " << media << endl;
-}
-
 
 int main() {
-    srand(static_cast<unsigned int>(std::time(0)));
-    int amostras = 1000;
-    int minimo = 0;
-    int tentativas = 1000;
-    vector<int> vetor;
-    
-	for (int i = 0; i < amostras; i++) {
-		int num = minimo + (rand() % (amostras - minimo + 1));
-		cout << num << " ";
-		vetor.push_back(num);
+    setlocale(LC_ALL, "");
+    for (int i = min_length; i <= max_length; i *= 10) {
+        test(i);
     }
 
-	sequencial(vetor, tentativas);
-	binaria(vetor, tentativas);
-    return 0;
 }
+
 
